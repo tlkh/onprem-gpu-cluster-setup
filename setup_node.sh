f7db2601744eb8e72eae2eae75ac6a2b5b0bb758
Timothy Liu <tlkh.xms@gmail.com>
2019-12-26 16:48:06 +0800

    Update setup_node.sh

--- setup_node.sh
+++ setup_node.sh
@@ -47,7 +47,7 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 
 apt-get update
-apt-get install -y nvidia-container-toolkit
+apt-get install -y nvidia-docker2
 
 echo -e "\nInstalling Tools and Telemetry Stack\n"
 
